5edf0d0fbe25fadbac179cc28637ddea6e51acd9
FedAdmin <>
2021-03-03 21:06:29 +0700

    update

--- src/constants/eggPriceContracts.ts
+++ src/constants/eggPriceContracts.ts
@@ -1,7 +1,7 @@
 const priceContracts: {cakeAddress: string, busdAddress: string, lpAddress:string} = {
-  cakeAddress: '0xf952fc3ca7325cc27d15885d37117676d25bfda6',
+  cakeAddress: '0xa63d087faad155d83a83a52c4ec640386db4fedb',
   busdAddress: '0xe9e7cea3dedca5984780bafc599bd69add087d56',
-  lpAddress: '0x19e7cbecdd23a16dfa5573df54d98f7caae03019'
+  lpAddress: '0x02D571f7d0Cfb96096856cFb5513f543F317Be79'
 }
 
 export default priceContracts
